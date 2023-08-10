553edceb389fa5cd482c99bc88a3276fc3880889
root <root@DESKTOP-1ULGF16.localdomain>
2023-08-10 16:34:37 -0400

    message

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,26 +1,49 @@
 import React, { useEffect, useState } from "react";
-import Counter from "../Counter/Counter.jsx";
-import c from "./App.module.css";
+import "./App.module.css";
 import axios from "axios";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Dashboard from "../Dashboard/Dashboard"
+import Register from "../Register/Register.jsx"
+import Login from "../Login/Login.jsx"
+
+
+
 
 const App = () => {
-  const [tasks, setTasks] = useState([]);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  const getTasks = () => {
-    axios.get("/api/tasks").then((res) => {
-      setTasks(res.data);
-    });
-  };
+  const setAuth = boolean => {
+    setIsAuthenticated(boolean);
+  }
 
-  useEffect(getTasks, []);
+  useEffect(() => {
+    checkAuthenticated()
 
-  const deleteTask = async (id) => {
-    await axios.delete(`/api/tasks/${id}`);
-    return getTasks();
-  };
+  }, [])
 
-  return (
-    <main>
+
+  const checkAuthenticated = async () => {
+    try {
+      const res = await fetch("http://localhost:8000/authentication/verify", {
+        method: "GET",
+        headers: { jwt_token: localStorage.token }
+
+      })
+      const parseRes = await res.json();
+      parseRes === true ? setIsAuthenticated(true) : setIsAuthenticated(false);
+
+    }
+    catch (error) {
+      console.error("Error message")
+    }
+  }
+
+
+
+
+
+  
+  return (    /*<main>
       <h2>Tasks</h2>
       <div className={c.tasks}>
         {tasks.length > 0 ? (
@@ -37,7 +60,49 @@ const App = () => {
       <h2>Counter</h2>
       <Counter />
     </main>
-  );
+  ;*/
+    <Router>
+
+      <div className="container" >
+      <Routes>
+        <Route 
+        exact path = "/login"
+        render = {props => 
+          !isAuthenticated ? (
+            <Login {...props} setAuth={setAuth} />
+          ) : (
+            <Link to="/dashboard" />
+          )
+        }
+        /> 
+
+        <Route 
+        exact path = "/register"
+        render = {props => 
+          !isAuthenticated ? (
+            <Register {...props} setAuth={setAuth} />
+          ) : (
+            <Link to="/dashboard" />
+          )
+        }
+        /> 
+
+        <Route 
+        exact path = "/dashboard"
+        render = {props => 
+          !isAuthenticated ? (
+            <Dashboard {...props} setAuth={setAuth} />
+          ) : (
+            <Link to="/login" />
+          )
+        }
+        /> 
+
+      </Routes>
+
+      </div>
+    </Router>
+  )
 };
 
 export default App;
