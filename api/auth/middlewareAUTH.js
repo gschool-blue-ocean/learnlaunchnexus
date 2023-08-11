54078d08550da8dd5f3695e180d44214b6bb30da
Sean Guerrero <seanguerrero549@gmail.com>
2023-08-11 16:24:01 -0700

    added secret to middlewareAUTH

--- api/auth/middlewareAUTH.js
+++ api/auth/middlewareAUTH.js
@@ -21,7 +21,7 @@ secret.
 use in our routes
     */
 
-    const payload = jwt.verify(token, process.env.secret);
+    const payload = jwt.verify(token, '123');
     console.log("1", payload);
 
     /*
