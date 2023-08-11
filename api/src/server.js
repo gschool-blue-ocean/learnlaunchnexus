1b3d9bf67e4000920f60bdc011c9ce339205becc
PatDMcG <110838464+PatDMcG@users.noreply.github.com>
2023-08-11 14:54:35 -0400

    updated to es

--- api/src/server.js
+++ api/src/server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const cors = require('cors');
+import express from 'express'
+import cors from 'cors'
  
 const app = express();
 const PORT = process.env.PORT || 3000;
