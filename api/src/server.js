0e18aaea1d07f51a89f5b128bd79e0271ccf3364
PatDMcG <110838464+PatDMcG@users.noreply.github.com>
2023-08-11 18:29:30 -0400

    Update server.js

--- api/src/server.js
+++ api/src/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3007;
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static("../client/src/dist")); 
+app.use(express.static("../../client/src/dist")); 
 
 
 import studentRoutes from './routes/students.js'
