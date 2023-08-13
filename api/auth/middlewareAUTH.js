268ff35a48a74580952ebb77726a96506dde86e8
Sean Guerrero <seanguerrero549@gmail.com>
2023-08-13 13:19:54 -0700

    changed position of try blick

--- api/auth/middlewareAUTH.js
+++ api/auth/middlewareAUTH.js
@@ -4,22 +4,22 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 export default async (req, res, next) => {
-  try {
-    const token = req.header("token");
-
-    if (!token) {
-      return res.status(403).send("not authorized");
-    }
-
-    /*
-    This verify method on the jwt module tells us weather the jwt is valid.
-Takes two args. First is the jwt given to us in the req.header, the second is our
-secret.
-
-    calling this payload because if it is verified it returns a payload we can
-use in our routes
-    */
-
+  const token = req.header("token");
+  
+  if (!token) {
+    return res.status(403).send("not authorized");
+  }
+  
+  /*
+  This verify method on the jwt module tells us weather the jwt is valid.
+  Takes two args. First is the jwt given to us in the req.header, the second is our
+  secret.
+  
+  calling this payload because if it is verified it returns a payload we can
+  use in our routes
+  */
+ 
+ try {
     const payload = jwt.verify(token, process.env.SECRET_KEY);
     console.log("1", payload);
 
