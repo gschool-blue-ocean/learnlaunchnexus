7edc15d63f15658b4c7753a3d9827a59e4d17ce1
PatDMcG <110838464+PatDMcG@users.noreply.github.com>
2023-08-13 20:02:30 -0400

    fixing login button

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -73,7 +73,7 @@ const Login = ({ setAuth }) => {
                     />
                     <br></br>
                     <h2></h2>
-                    <button id='logbtn' class="btn btn-success btn-block">Login</button>
+                    <button onclick={onClick(e)} id='logbtn' className="btn btn-success btn-block">Login</button>
                   </form>
                   <br></br>
                   </div>
