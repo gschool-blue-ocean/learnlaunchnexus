c6b140d86c85c977831fd05e4975a5a7b9313946
PatDMcG <110838464+PatDMcG@users.noreply.github.com>
2023-08-15 16:06:01 -0400

    added key

--- src/components/student/Submission.jsx
+++ src/components/student/Submission.jsx
@@ -3,12 +3,12 @@ import React, {useEffect, useState} from 'react';
 const Submission = ({assignment}) => {
 
 return (
-    <>
+    <div key={assignment.assignment_id}>
     <h1>{assignment.name}</h1>
     <h1>{assignment.status}</h1>
     <h1>{assignment.submission_time}</h1>
     <h1>{assignment.feedback}</h1>
-    </>
+    </div>
 )
 
 
