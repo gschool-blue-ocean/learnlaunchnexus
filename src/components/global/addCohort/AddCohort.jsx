e500d38bd98d52dfc8dca9f20f3b4405eec5eef4
PatDMcG <110838464+PatDMcG@users.noreply.github.com>
2023-08-23 09:33:59 -0400

    added new functionality for admin
    sean please review for the dropdown to ensure its implemented properly

---
+++ src/components/global/addCohort/AddCohort.jsx
@@ -0,0 +1,84 @@
+import React, {useState} from 'react'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const AddCohort = () => {
+    
+
+    const [inputs, setInputs] = useState({
+        start_date : "", //date
+        end_date: "", //date
+        name: "" //text
+    })
+    
+    /*
+    router.post('/', async (req, res) => {
+    const { start_date, end_date, name } = req.body;
+    try {
+        const result = await pool.query("INSERT INTO cohort (start_date, end_date, name) VALUES ($1, $2, $3) RETURNING *", 
+                                        [start_date, end_date, name]);
+        res.json(result.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+         res.status(500).json(err.message);
+    }
+});*/
+
+const onSubmitForm = async (e) => {
+    console.log('inside onSubmitForm')
+    e.preventDefault();
+    const { start_date, end_date, name } = inputs
+    const body = {start_date, end_date, name}
+    try {
+          const res = await fetch(`${import.meta.env.VITE_API}/cohort`, {
+            headers: { 'Content-Type': 'application/json' },
+            method: "POST",
+            body: JSON.stringify(body)
+
+          });
+          const parseData = await res.json();
+          toast.success(`added a cohort`)
+        } catch (err) {
+          console.error(err.message);
+          toast.error(`Failed to add a cohort`)
+        }
+    }
+
+const onChange = (e) => {
+setInputs({ ...inputs ,[e.target.name]: e.target.value })
+}
+return (
+<>
+    <ToastContainer />
+    <form>
+    <h1> Add a Start Date</h1>
+    <input id="start_date"
+              type="date"
+              name="start_date"
+              value={inputs.start_date}
+              onChange={e => onChange(e)}
+              className="undefined"
+            />
+    <h1> Add a End Date</h1>
+    <input id="end_date"
+              type="date"
+              name="end_date"
+              value={inputs.end_date}
+              onChange={e => onChange(e)}
+              className="undefined"
+            />   
+     <h1> Add a End Date</h1>
+    <input id="name"
+              type="text"
+              name="name"
+              value={inputs.name}
+              onChange={e => onChange(e)}
+              className="undefined"
+            />
+    
+    <button onClick={onSubmitForm} id='submitAdmin' className="Submission">Submit</button>
+    </form>
+</>
+)
+}
+export default AddCohort;
---
+++ src/components/global/assignAssignment/AssignAssignment.jsx
@@ -0,0 +1,107 @@
+import React, {useState} from 'react'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const AssignCohort = () => {
+    const [cohorts, setCohorts] = useState([]);
+    const [unassigned, setUnassigned] = useState([]);
+
+    useEffect(() => {
+
+        const getCohortData = async () => {
+    
+          try {
+            const Cohortres = await fetch(`${import.meta.env.VITE_API}/cohort`, {
+              method: "GET",
+            });
+    
+            const parseCohortData = await Cohortres.json();
+            setCohorts(parseCohortData)
+    
+            const Unassignedres = await fetch(`${import.meta.env.VITE_API}/users/unassigned`, {
+              method: "GET",
+            });
+    
+            const parseUnassignedData = await Unassignedres.json();
+            setUnassigned(parseUnassignedData)
+    
+    
+          } catch (err) {
+            console.error(err.message);
+          }
+    
+        }
+        getCohortData();
+      }, []);
+
+    const [inputs, setInputs] = useState({
+        cohort_id: 0,
+         user_id: 0
+    })
+    
+    /*
+    router.post('/', async (req, res) => {
+    const { cohort_id, user_id, desired_location, location } = req.body;
+    try {
+        const result = await pool.query("INSERT INTO student (cohort_id, user_id, desired_location, location) VALUES ($1, $2, $3, $4) RETURNING *",
+            [cohort_id, user_id, desired_location, location]);
+        res.json(result.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+res.status(500).json(err.message);    }
+});*/
+
+const onSubmitForm = async (e) => {
+    console.log('inside onSubmitForm')
+    e.preventDefault();
+    const { cohort_id, user_id } = inputs
+    const body = {cohort_id, user_id}
+    try {
+          const res = await fetch(`${import.meta.env.VITE_API}/cohort`, {
+            headers: { 'Content-Type': 'application/json' },
+            method: "POST",
+            body: JSON.stringify(body)
+
+          });
+          const parseData = await res.json();
+          toast.success(`assigned a student`)
+        } catch (err) {
+          console.error(err.message);
+          toast.error(`Failed to assign a student`)
+        }
+    }
+
+const onChange = (e) => {
+setInputs({ ...inputs ,[e.target.name]: e.target.value })
+}
+return (
+<>
+    <ToastContainer />
+    <form>
+    <h1> Select a cohort</h1>
+    <select onChange={onChange} name="cohort_id">
+        {(cohorts.length > 0) && cohorts.map((cohort) => {
+                               
+            return (
+                                        <option value={cohort.id}>{cohort.name}</option>
+                                    )
+                                
+        })}
+    </select>
+    <h1> Select an unassigned user</h1>
+    <select onChange={onChange} name="user_id">
+        {(unassigned.length > 0) && unassigned.map((potential) => {
+                               
+            return (
+                                        <option value={potential.id}>{potential.first_name} {potential.last_name}</option>
+                                    )
+                                
+        })}
+    </select>
+    
+    <button onClick={onSubmitForm} id='submitAdmin' className="Submission">Submit</button>
+    </form>
+</>
+)
+}
+export default AssignCohort;
